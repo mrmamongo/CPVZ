3f74313b50e3082baf61d6d79411e5c9da61938d
nyurlov <mrmamongo@gmail.com>
2022-08-10 09:22:46 +0300

    add submodules

--- Configure.cmake
+++ Configure.cmake
@@ -1,12 +1,8 @@
-option(CPVZ_ENABLE_TESTS "Enable tests" ON)
+option(CPVZ_ENABLE_TESTS "Enable tests" OFF)
 
 set(CMAKE_CXX_FLAGS         "")
 set(CMAKE_CXX_FLAGS_DEBUG   "")
 set(CMAKE_CXX_FLAGS_RELEASE "")
-
-set(build_static_lib ON)
-set(lib_utc_datetime ON)
-add_subdirectory(third-party/easylogging)
 
 string(APPEND CMAKE_CXX_FLAGS " -Wall -Wextra -pedantic")
 string(APPEND CMAKE_CXX_FLAGS " -Wconversion -Wunreachable-code -Wuninitialized -Wold-style-cast")
@@ -17,16 +13,3 @@ string(APPEND CMAKE_CXX_FLAGS " -fPIC")
 
 string(APPEND CMAKE_CXX_FLAGS_DEBUG " -g -Og")
 string(APPEND CMAKE_CXX_FLAGS_RELEASE " -O3 -Werror")
-
-include(FetchContent)
-FetchContent_Declare(
-        googletest
-        URL https://github.com/google/googletest/archive/5376968f6948923e2411081fd9372e71a59d8e77.zip
-)
-FetchContent_MakeAvailable(googletest)
-
-set(BUILD_SHARED_LIBS ON)
-set(HTTPLIB_REQUIRE_OPENSSL ON)
-set(HTTPLIB_REQUIRE_ZLIB ON)
-set(HTTPLIB_COMPILE ON)
-add_subdirectory(third-party/httplib)
